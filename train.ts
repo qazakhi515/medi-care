bccce1fa331256376be28494aedee0521b0ceff7
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-04-01 16:14:11 +0900

    feat: integration Graph QL module and built first Graph QL api

---
+++ train.ts
@@ -0,0 +1,18 @@
+// TASK ZJ:
+// Shunday function yozing, u berilgan array ichidagi
+// raqamlarni qiymatini hisoblab qaytarsin.
+// MASALAN: reduceNestedArray([1, [1, 2, [4]]]); return 8;
+// Yuqoridagi misolda, array nested bo'lgan holdatda ham,
+// bizning function ularning yig'indisini hisoblab qaytarmoqda.
+function reduceNestedArray(arr: (number | any[])[]): number {
+	let sum = 0;
+	for (const item of arr) {
+		if (Array.isArray(item)) {
+			sum += reduceNestedArray(item);
+		} else {
+			sum += item;
+		}
+	}
+	return sum;
+}
+console.log(reduceNestedArray([1, [1, 2, [4]]])); // 8
