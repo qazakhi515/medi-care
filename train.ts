c8bd726c19ea9288c5e68de548aee278ccc12889
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-04-03 10:44:32 +0500

    feat: task has done

--- train.ts
+++ train.ts
@@ -4,15 +4,30 @@
 // MASALAN: reduceNestedArray([1, [1, 2, [4]]]); return 8;
 // Yuqoridagi misolda, array nested bo'lgan holdatda ham,
 // bizning function ularning yig'indisini hisoblab qaytarmoqda.
-function reduceNestedArray(arr: (number | any[])[]): number {
-	let sum = 0;
-	for (const item of arr) {
-		if (Array.isArray(item)) {
-			sum += reduceNestedArray(item);
-		} else {
-			sum += item;
+// function reduceNestedArray(arr: (number | any[])[]): number {
+// 	let sum = 0;
+// 	for (const item of arr) {
+// 		if (Array.isArray(item)) {
+// 			sum += reduceNestedArray(item);
+// 		} else {
+// 			sum += item;
+// 		}
+// 	}
+// 	return sum;
+// }
+// console.log(reduceNestedArray([1, [1, 2, [4]]])); // 8
+
+function startCounter() {
+	let count = 1;
+
+	const interval = setInterval(() => {
+		console.log(count);
+		count++;
+
+		if (count > 5) {
+			clearInterval(interval);
 		}
-	}
-	return sum;
+	}, 1000);
 }
-console.log(reduceNestedArray([1, [1, 2, [4]]])); // 8
+
+startCounter();
