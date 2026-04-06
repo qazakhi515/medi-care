9d85be54a44a48c23c961d86d1f331a5c8f22066
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-04-06 15:27:40 +0500

    feat: develop global error graph ql error handling system

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -18,7 +18,7 @@ export class MemberService {
 			return result;
 		} catch (err) {
 			console.log('Error, Service.model:', err);
-			throw new BadRequestException(err);
+			throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
 		}
 	}
 
