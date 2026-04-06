6248382ed7150f2d5ac615353ed125f3b8b11180
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-04-06 09:02:15 +0500

    feat:integration validation pipes logic

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -3,6 +3,7 @@ import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
+
 @Module({
 	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }])],
 	providers: [MemberResolver, MemberService],
--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,31 +1,36 @@
-import { Mutation, Resolver, Query } from '@nestjs/graphql';
+import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 
 @Resolver()
 export class MemberResolver {
 	constructor(private readonly memberService: MemberService) {}
 
 	@Mutation(() => String)
-	public async signup(): Promise<string> {
-		console.log('Mutation: signup ');
+	@UsePipes(ValidationPipe)
+	public async signup(@Args('input') input: MemberInput): Promise<string> {
+		console.log('Mutation: signup');
+		console.log('input:', input);
 		return this.memberService.signup();
 	}
 
 	@Mutation(() => String)
-	public async login(): Promise<string> {
-		console.log('Mutation: login ');
+	@UsePipes(ValidationPipe)
+	public async login(@Args('input') input: LoginInput): Promise<string> {
+		console.log('Mutation: login');
 		return this.memberService.login();
 	}
 
 	@Mutation(() => String)
 	public async updateMember(): Promise<string> {
-		console.log('Mutation: updateMember ');
+		console.log('Mutation: updateMember');
 		return this.memberService.updateMember();
 	}
 
 	@Query(() => String)
 	public async getMember(): Promise<string> {
-		console.log('Mutation: getMember ');
-		return 'getMember executed';
+		console.log('Mutation: getMember');
+		return this.memberService.getMember();
 	}
 }
