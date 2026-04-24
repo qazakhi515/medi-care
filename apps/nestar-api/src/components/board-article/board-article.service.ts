f40cf88fb9ece7f04aaa9ee147fac1e386b9a8cf
Akhmadjon Usmonov <qazakhi515@gmail.com>
2026-04-24 10:23:19 +0500

    feat: create follow related resolver and service model

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -61,7 +61,8 @@ export class BoardArticleService {
 				await this.boardArticleStatsEditor({ _id: articleId, targetKey: 'articleViews', modifier: 1 });
 				targetBoardArticle.articleViews++;
 			}
-
+			const likeInput = { memberId, likeRefId: articleId, likeGroup: LikeGroup.ARTICLE };
+			targetBoardArticle.meLiked = await this.likeService.checkLikeExistence(likeInput);
 			// meLiked
 		}
 		targetBoardArticle.memberData = await this.memberService.getMember(null, targetBoardArticle.memberId);
